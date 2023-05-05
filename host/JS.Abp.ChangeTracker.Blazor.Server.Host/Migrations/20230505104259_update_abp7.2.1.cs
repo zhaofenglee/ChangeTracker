951f8f7d6a6277a5f2da50ba5aeba8e9c54cd722
lizhaofeng <zhaof_li@163.com>
2023-05-05 19:02:30 +0800

    Upgrade the ABP project version to 7.2.1

---
+++ host/JS.Abp.ChangeTracker.Blazor.Server.Host/Migrations/20230505104259_update_abp7.2.1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JS.Abp.ChangeTracker.Blazor.Server.Host.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateabp721 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ChangeTrackerChangeLogs",
+                table: "ChangeTrackerChangeLogs");
+
+            migrationBuilder.RenameTable(
+                name: "ChangeTrackerChangeLogs",
+                newName: "AbpChangeLogs");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LastPasswordChangeTime",
+                table: "AbpUsers",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ShouldChangePasswordOnNextLogin",
+                table: "AbpUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AbpChangeLogs",
+                table: "AbpChangeLogs",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "AbpUserDelegations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TenantId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    SourceUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TargetUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AbpUserDelegations", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AbpUserDelegations");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AbpChangeLogs",
+                table: "AbpChangeLogs");
+
+            migrationBuilder.DropColumn(
+                name: "LastPasswordChangeTime",
+                table: "AbpUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ShouldChangePasswordOnNextLogin",
+                table: "AbpUsers");
+
+            migrationBuilder.RenameTable(
+                name: "AbpChangeLogs",
+                newName: "ChangeTrackerChangeLogs");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ChangeTrackerChangeLogs",
+                table: "ChangeTrackerChangeLogs",
+                column: "Id");
+        }
+    }
+}
--- src/JS.Abp.ChangeTracker.Domain/ChangeTrackerDbProperties.cs
+++ src/JS.Abp.ChangeTracker.Domain/ChangeTrackerDbProperties.cs
@@ -1,10 +1,12 @@
-﻿namespace JS.Abp.ChangeTracker;
+﻿using Volo.Abp.Data;
+
+namespace JS.Abp.ChangeTracker;
 
 public static class ChangeTrackerDbProperties
 {
-    public static string DbTablePrefix { get; set; } = "ChangeTracker";
+    public static string DbTablePrefix { get; set; } =  AbpCommonDbProperties.DbTablePrefix;
 
-    public static string? DbSchema { get; set; } = null;
+    public static string? DbSchema { get; set; } = AbpCommonDbProperties.DbSchema;
 
     public const string ConnectionStringName = "ChangeTracker";
 }
